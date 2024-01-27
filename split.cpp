8fda5aad69573938f6c8cccd14b69f4299362c22
Jerry Zhang <jjzhang@usc.edu>
2024-01-27 06:37:57 +0000

    all done with p1

--- split.cpp
+++ split.cpp
@@ -11,6 +11,9 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <iostream>
+using namespace std;
+
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +21,47 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  Node* curr = in;
+  if (in == nullptr){
+    if (odds != nullptr){
+      odds->next = nullptr;
+    }
+    if (evens != nullptr){
+      evens->next = nullptr;
+    }
+    return;
+  }
+  else{
+    if (curr->value % 2 == 1){
+      if (odds == nullptr){
+        odds = in;
+        in = nullptr;
+        Node* oddsTemp = odds;
+        //cout << "odds" << odds->value << endl;
+        split(curr->next, oddsTemp, evens);
+      }
+      else{
+        odds->next = curr;
+        odds = odds->next;
+        //cout << "odds" << odds->value << endl;
+        split(curr->next, odds, evens);
+      }
+    }
+    else if (curr->value % 2 == 0){
+      if (evens == nullptr){
+        evens = in;
+        in = nullptr;
+        Node* evensTemp = evens;
+       // cout << "evens" << evens->value << endl;
+        split(curr->next, odds, evensTemp);
+      }
+      else{
+        evens->next = curr;
+        evens = evens->next;
+       // cout << "evens" << evens->value << endl;
+        split(curr->next, odds, evens);
+      }
+    }
+  }
+  return;
 }
-
-/* If you needed a helper function, write it here */
--- split.h
+++ split.h
@@ -1,5 +1,7 @@
 #ifndef SPLIT_H
 #define SPLIT_H
+#include <iostream>
+using namespace std;
 
 
 struct Node 
